c349469bd2c71e83b1d7c04096e68fc507b1ef14
Shaunbag <shaunbag@hotmail.com>
2020-11-25 10:42:04 +0000

    app final

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Business from '../Business/Business';
-import BusinessList from '../BusinessList/BuisnessList';
+import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
 function App() {
